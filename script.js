81163aa7dab81c2467134c2655b3d44ebad2bc07
Artem <artemsch1711@gmail.com>
2025-02-26 21:39:47 +0300

    Stability addae + button

--- script.js
+++ script.js
@@ -12,9 +12,15 @@ let clouds = document.getElementById('clouds');
 let humidity = document.getElementById('humidity');
 let pressure = document.getElementById('pressure');
 let main = document.querySelector('main');
+let result = document.querySelector('.result');
 
-form.addEventListener("submit", (e) => {
+const submit = document.getElementById('submit')
+
+submit.addEventListener("click", (e) => {
     e.preventDefault();  
+    if (result.classList.contains('hidden')) {
+        result.classList.remove('hidden');
+    }
     if(valueSearch.value != ''){
         searchWeather();
     }
@@ -30,7 +36,7 @@ const searchWeather = () => {
             if(data.cod == 200){
                 city.querySelector('h2').innerHTML = data.name;
                 // city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
-                temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+                // temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
                 temperature.querySelector('span').innerText = data.main.temp;
                 description.innerText = data.weather[0].description;
 
@@ -54,9 +60,9 @@ const searchWeather = () => {
     xhr.send();
 }
 
-// search Default
-const initApp = () => {
-    valueSearch.value = 'Washington';
-    searchWeather();
-}
-initApp();
+// // search Default
+// const initApp = () => {
+//     valueSearch.value = 'Washington';
+//     searchWeather();
+// }
+// initApp();
