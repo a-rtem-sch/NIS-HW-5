9142bf944981feb15a7acfc7c8582d3dee6142ed
Artem Shcherbakov <artemsch1711@gmail.com>
2025-02-27 16:51:19 +0300

    Баг при вводе пустой строки

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ submit.addEventListener("click", (e) => {
     if(valueSearch.value != ''){
         searchWeather();
     }
+    else {
+        cancelTimeout()
+    }
 });
 
 const searchWeather = () => {
